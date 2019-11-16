419c563daa3ce84e95ace3cad6d09401bcf1b31b
Manuel Rodríguez <manuel.rodriguezs@gmail.com>
2019-11-16 23:01:28 +0100

    Visual

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # Shopping list
 Symfony 4 test
 
-`docker-compose up -d`
+```
+docker-compose up -d
+```
 
 ```
-docker exec shopping-list_php_1 \
-       composer install \
-    && bin/console doctrine:database:create
+docker exec shopping-list_php_1 composer install
+```
+```
+docker exec shopping-list_php_1 php bin/console doctrine:database:create
+```
+```
+docker exec shopping-list_php_1 php bin/console doctrine:migrations:migrate -q
 ```
---
+++ src/Entity/Item.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="items")
+ * @ORM\Entity(repositoryClass="App\Repository\ItemRepository")
+ */
+class Item
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=30, unique=true)
+     */
+    private $key;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getKey(): ?string
+    {
+        return $this->key;
+    }
+
+    public function setKey(string $key): self
+    {
+        $this->key = $key;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
